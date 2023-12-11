8a545fbf79e5a39255512464219a5a8fedbec956
Miho Rosenberg <131460356+MihoRosenberg@users.noreply.github.com>
2023-12-11 23:15:37 +0000

    [UPDATE]

--- src/tasks/task-4-model-deployment/streamlit/app/pages/6-CentralRegion_Weather.py
+++ src/tasks/task-4-model-deployment/streamlit/app/pages/6-CentralRegion_Weather.py
@@ -41,19 +41,19 @@ st.write("""
     """)
 
 st.write("""
-    <B>Problem Statement:</B>
+    Problem Statement:
     The aftermath of flooding in Bangladesh results in immediate and long-term challenges, including loss of lives, destruction of crops, damage to infrastructure, and displacement of communities. 
             Timely and accurate flood prediction and waterbody forecasting are crucial for reducing the impact of floods, enabling better disaster preparedness, and facilitating effective resource allocation.
     """)
 
 st.write("""
-    <B>Model Overview:</B>
+    Model Overview:
     The study predicts the daily average precipitation for four divisions in Bangladesh- Dhaka, Khulna, Mymensingh, and Narayanganj- using RandomForest Regressor.
     The model’s performance was evaluated with the R2 score: 0.71, mean squared error: 19.50, and mean absolute error: 2.33.
     """)
 
 st.write("""
-    <B>Further Consideration: </B>
+    Further Consideration:
     A possible way to reduce the negative effects and prevent further harm is to use a real-time prediction mechanism. 
     This would allow for timely and accurate responses to the situation. To achieve this, an automated end-to-end ML pipeline is suggested. The pipeline would collect data through API, transform it into a suitable format, and deliver a near real-time prediction.
     """)
